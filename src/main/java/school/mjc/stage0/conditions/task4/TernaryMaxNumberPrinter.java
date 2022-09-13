904bc10788f7aa5282f94b3d6463b28fe10bd51e
mukhamedovislam <bozuy24@gmail.com>
2022-09-13 11:27:16 +0400

    Add Ternary Operator Solutions

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+	    int result = 0;
+    if (first >= second){
+      result = first >= third ? first : third;
+    } else {
+      result = second >= third ? second : third;
+    }
+    System.out.println(result);
+
     }
 }
