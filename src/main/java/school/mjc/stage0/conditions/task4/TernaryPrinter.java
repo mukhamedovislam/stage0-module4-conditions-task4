d426272d34f420a26fc89943ff43673b0b098918
mukhamedovislam <bozuy24@gmail.com>
2022-09-13 12:18:07 +0400

    Add Ternary Operator Solution #3

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
@@ -2,8 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryPrinter {
     public void printWhichIsBigger(int first, int second) {
-	int result = 0;
-	result = first >= second ? first : second;
+	String result = "";
+	result = first >= second ? "first" : "second";
 	System.out.println(result);
     }
 }
