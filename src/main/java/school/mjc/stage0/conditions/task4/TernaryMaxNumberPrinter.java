ab6d21e68941305b2fb4efc35f7c72b0e2bc7d14
mukhamedovislam <bozuy24@gmail.com>
2022-09-14 09:48:05 +0400

    Add completed ternary conditions tasks

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,13 +2,10 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
-	    int result = 0;
-    if (first >= second){
-      result = first >= third ? first : third;
-    } else {
-      result = second >= third ? second : third;
-    }
-    System.out.println(result);
-
+	    System.out.println(first > second && first > third ? 
+			    first : 
+			    second > first && second > third ? 
+			    second :
+			    third);
     }
 }
